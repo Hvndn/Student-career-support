f68474ef491f3b60c607dbe3262d180ff57300bd
Hvndn <dienhovan2000@gmail.com>
2026-03-13 08:26:14 +0700

    update dashboard frontend

--- src/main/java/com/fivecore/jobportal/HomeController.java
+++ src/main/java/com/fivecore/jobportal/HomeController.java
@@ -1,6 +1,7 @@
 package com.fivecore.jobportal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,13 +12,23 @@ public class HomeController {
         return "index";
     }
 
+
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+
+        // Demo dữ liệu user (sau này lấy từ DB)
+        model.addAttribute("user", "Diện");
+
         return "dashboard";
     }
+
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+    @GetMapping("/jobs")
+public String jobs() {
+    return "jobs";
+}
 }
