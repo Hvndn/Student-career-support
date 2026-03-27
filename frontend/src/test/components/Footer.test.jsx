107fd7f99cde1d3ac4ae0197396730ab9cbc0340
unknown <vantung2k44@gmail.com>
2026-03-27 09:43:02 +0700

    test: Move frontend tests to src/test folder

--- frontend/src/test/components/Footer.test.jsx
+++ frontend/src/test/components/Footer.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import Footer from './Footer';
+import Footer from '../../components/Footer';
 
 describe('Footer Component', () => {
     beforeEach(() => {
--- frontend/src/test/components/Navbar.test.jsx
+++ frontend/src/test/components/Navbar.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from '../../components/Navbar';
 
 describe('Navbar Component', () => {
     const renderNavbar = (route = '/') => {
